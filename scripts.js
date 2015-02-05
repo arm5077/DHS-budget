925765c2c699807fc07dcc7ce6a8bfdd63e7db33
Andrew McGill <arm5077@gmail.com>
2015-02-05 15:34:06 -0500

    Split into separate css and js

---
+++ scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	
+	
+	
+	var height = $("#main-text").height();
+	// Move title up to top within 10% of scroll over main text blob
+	$("#title").attr("data-" + height * .05, "top: " + getCenterFixedObject($("#title")) + "px");
+	$("#title").attr("data-" + height * .15, "top: 10px");
+		
+	$("#p1").attr("data-"+ height * .1, "opacity: 0; top: " + $(window).height() + "px");
+	$("#p1").attr("data-"+ height * .3, "opacity: 1; top: " + (10 + $("#title").outerHeight() + 50) + "px");
+		
+	// Start sliding and fading title away and moving up p1
+	$("#title").attr("data-" + height * .4, "top: 10px");
+	$("#title").attr("data-" + height * .6, "top: -" + ( $("#title").outerHeight() + 20 ) + "px");
+	$("#p1").attr("data-"+ height * .4, "opacity: 1; top: " + (10 + $("#title").outerHeight() + 50) + "px");
+	$("#p1").attr("data-"+ height * .6, "opacity: 1; top: 10px");
+		
+	$("#p2").attr("data-"+ height * .4, "opacity: 0; top: " + $(window).height() + "px");
+	$("#p2").attr("data-"+ height * .6, "opacity: 1; top: " + ( 10 + $("#p1").outerHeight() + 10 ) + "px");
+	
+	// Roll it all up!
+	$("#p1").attr("data-"+ height * .75, "opacity: 1; top: 10px");
+	$("#p1").attr("data-"+ height * .95, "opacity: 1; top: -" + ( $("#p2").outerHeight() + 10 + $("#p1").outerHeight() + 10 ));
+	$("#p2").attr("data-"+ height * .75, "opacity: 1; top: " + ( 10 + $("#p1").outerHeight() + 10 ) + "px");	
+	$("#p2").attr("data-"+ height * .95, "opacity: 1; top: -" + ( $("#p2").outerHeight() + 10 ) + "px");	
+	
+	
+	
+	var s = skrollr.init();
+		
+	document.location = "#";
+	
+});
+
+
+function getCenterFixedObject( obj ){
+	return ( $(window).height() - $(obj).outerHeight() ) / 2;
+};
